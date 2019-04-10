2695f91ee3018da656aefa5239ddce929bc81631
Kyle Bradford <kyle@watchtower.ai>
2019-04-10 09:37:20 -0700

    refactor: move language code into greetingPrefix

--- hello/hello.go
+++ hello/hello.go
@@ -16,16 +16,19 @@ func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	prefix := englishPrefix
+	return greetingPrefix(language) + name
+}
 
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case spanish:
 		prefix = spanishPrefix
 	case french:
 		prefix = frenchPrefix
+	default:
+		prefix = englishPrefix
 	}
-
-	return prefix + name
+	return
 }
 
 func main() {
