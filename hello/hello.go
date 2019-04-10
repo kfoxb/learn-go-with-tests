ac0ba4a556ce9dc962f1318a24a917594752bd76
Kyle Bradford <kyle@watchtower.ai>
2019-04-09 19:12:21 -0700

    feat: add Spanish language flag

--- hello/hello.go
+++ hello/hello.go
@@ -4,16 +4,23 @@ import (
 	"fmt"
 )
 
-const prefix = "Hello, "
+const prefixEnd = ", "
+const englishPrefix = "Hello" + prefixEnd
+const spanishPrefix = "Hola" + prefixEnd
+const spanish = "Spanish"
 
 // Hello returns "Hello, world"
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+	prefix := englishPrefix
+	if language == spanish {
+		prefix = spanishPrefix
 	}
 	return prefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("World", ""))
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -13,14 +13,20 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("say hello to people by name", func(t *testing.T) {
-		got := Hello("Kyle")
+		got := Hello("Kyle", "")
 		want := "Hello, Kyle"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when given an empty string", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("works in Spanish", func(t *testing.T) {
+		got := Hello("Kyle", "Spanish")
+		want := "Hola, Kyle"
 		assertCorrectMessage(t, got, want)
 	})
 }
